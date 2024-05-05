ebc37ee95736f6edce58c9fc36ca7463bcabffa8
Beela <nabilaalhassan70@gmail.com>
2024-05-05 12:18:35 +0000

    health check API

--- Backend/app.py
+++ Backend/app.py
@@ -7,5 +7,11 @@ app = Flask(__name__)
 def index():
     return send_file('index.html')
 
+    # Health Check
+@app.route('/api/health-check', methods=['GET'])
+def health_check():
+    return jsonify({'status': 'OK'})
+
+
 if __name__ == '__main__':
     app.run(debug=True)
