a6d1d7df16b256632bf9c5a43c60fcbb34df7a79
Beela <nabilaalhassan70@gmail.com>
2024-05-05 12:13:42 +0000

    .

--- Backend/app.py
+++ Backend/app.py
@@ -1,7 +1,11 @@
-from flask import Flask
-
+from flask import Flask, send_file
 
 app = Flask(__name__)
+
+# Route to serve the index.html file from the root directory
+@app.route('/')
+def index():
+    return send_file('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
