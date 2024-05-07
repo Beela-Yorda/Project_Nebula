634fdc6184b8e0f722aafe7b153e903cabd64a65
Beela-Yorda <nabilaalhassan70@gmail.com>
2024-05-07 12:49:41 +0000

    project structure updated

--- Backend/app.py
+++ Backend/app.py
@@ -1,17 +1,22 @@
-from flask import Flask, send_file
+from flask import Flask, send_file, jsonify
+import os
+from flask_cors import CORS
+
 
 app = Flask(__name__)
+cors = CORS(app, origin='*')
 
 # Route to serve the index.html file from the root directory
-@app.route('/')
+@app.route("/", methods=['GET'])
 def index():
-    return send_file('index.html')
+    frontend_path = os.path.join(os.getcwd(), 'frontend', 'index.html')
+    return send_file(frontend_path)
 
-    # Health Check
+# Health Check
 @app.route('/api/health-check', methods=['GET'])
 def health_check():
     return jsonify({'status': 'OK'})
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
+
