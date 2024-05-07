6cbaf9080163ee06be4af4b701f829b52d04831f
Beela-Yorda <nabilaalhassan70@gmail.com>
2024-05-07 16:25:10 +0000

    configuring database

--- Backend/app.py
+++ Backend/app.py
@@ -1,6 +1,8 @@
 from flask import Flask, send_file, jsonify
 import os
 from flask_cors import CORS
+import mysql.connector
+#from config import DATABASE
 
 
 app = Flask(__name__)
@@ -16,6 +18,11 @@ def index():
 @app.route('/api/health-check', methods=['GET'])
 def health_check():
     return jsonify({'status': 'OK'})
+
+# Function to establish MySQL database connection
+#def connect_to_database():
+    #return mysql.connector.connect(**DATABASE)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
