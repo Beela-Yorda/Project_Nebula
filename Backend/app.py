0151dcd9274eb7321bf4ebfb8d066a0c10f83bdf
Beela-Yorda <nabilaalhassan70@gmail.com>
2024-05-10 10:50:27 +0000

    testing db connection

--- Backend/app.py
+++ Backend/app.py
@@ -1,6 +1,5 @@
 from flask import Flask, send_file, jsonify
 from flask_cors import CORS
-import mysql.connector
 from flask_mysqldb import MySQL
 from dotenv import load_dotenv
 import os
@@ -9,33 +8,21 @@ import os
 load_dotenv()
 
 app = Flask(__name__)
-cors = CORS(app, origin='*')
+CORS(app)  # Enable CORS for all routes
 
 # MySQL Configuration
-DATABASE = {
-    'host': os.getenv('DATABASE_HOST'),
-    'user': os.getenv('DATABASE_USER'),
-    'password': os.getenv('DATABASE_PASSWORD'),
-    'database': os.getenv('DATABASE_NAME')
-}
+app.config['MYSQL_HOST'] = os.getenv('DATABASE_HOST')
+app.config['MYSQL_USER'] = os.getenv('DATABASE_USER')
+app.config['MYSQL_PASSWORD'] = os.getenv('DATABASE_PASSWORD')
+app.config['MYSQL_DB'] = os.getenv('DATABASE_NAME')
 
 # Initialize MySQL
 mysql = MySQL(app)
 
-# Function to establish MySQL database connection
-def connect_to_database():
-    return mysql.connector.connect(**DATABASE)
-
-# Function to close database connection
-def close_database_connection(connection, cursor):
-    if connection.is_connected():
-        cursor.close()
-        connection.close()
-
 # Route to serve the index.html file from the root directory
 @app.route("/", methods=['GET'])
 def index():
-    frontend_path = os.path.join(os.getcwd(),'index.html')
+    frontend_path = os.path.join(os.getcwd(), 'index.html')
     return send_file(frontend_path)
 
 # Health Check
@@ -47,14 +34,14 @@ def health_check():
 @app.route('/api/test-db-connection', methods=['POST'])
 def test_db_connection():
     try:
-        db_connection = connect_to_database()
+        db_connection = mysql.connect()
         cursor = db_connection.cursor()
         cursor.execute("SELECT nebula FROM cohort")  # Corrected query
-        close_database_connection(db_connection, cursor)
+        cursor.close()
+        db_connection.close()
         return jsonify({'message': 'Database connection test successful'})
-    except mysql.connector.Error as err:
+    except Exception as err:
         return jsonify({'error': str(err)}), 500
-
 
 if __name__ == "__main__":
     app.run(debug=True)
